c0486ff3bfce80602507f57eadd0e71674d42171
Waggle <tsijesuswlg@gmail.com>
2017-04-10 05:27:36 +0000

    Reset Input data after inputting
    Arduino now resets Input and Delay data after inputting

--- ps2_controller/ps2_controller.ino
+++ ps2_controller/ps2_controller.ino
@@ -942,8 +942,26 @@ void pressButtons() {
           serial_rx_buffer_controller[6] = 0x7F;
           serial_rx_buffer_controller[7] = 0x00;
           serial_rx_buffer_controller[8] = 0x00;
-          //serial_rx_buffer_controller[9] = 0xFF;
-          //serial_rx_buffer_controller[10] = 0xFF;
+          //serial_rx_buffer_controller[9] = 0x00;
+          //serial_rx_buffer_controller[10] = 0x00;
+
+          for (serial_rx_buffer_counter = 0; serial_rx_buffer_counter < sizeof(serial_rx_buffer_controller); serial_rx_buffer_counter++) {
+            Serial.write(serial_rx_buffer_controller[serial_rx_buffer_counter]); //  This line writes the serial data back to the computer as a way to check if the Arduino isn't interpreting wrong values
+            //  Invert 0 to 255 and vice versa to make it easier to determine which commands to enable or not, and their values
+            serial_rx_buffer_inverted_controller[serial_rx_buffer_counter] = (255 - serial_rx_buffer_controller[serial_rx_buffer_counter]);
+          }
+
+          //  Reset delay to 0
+          serial_rx_buffer_controller[1] = 0x00;
+          serial_rx_buffer_controller[2] = 0x00;
+          serial_rx_buffer_controller[3] = 0x7F;
+          serial_rx_buffer_controller[4] = 0x7F;
+          serial_rx_buffer_controller[5] = 0x7F;
+          serial_rx_buffer_controller[6] = 0x7F;
+          serial_rx_buffer_controller[7] = 0x00;
+          serial_rx_buffer_controller[8] = 0x00;
+          serial_rx_buffer_controller[9] = 0x00;
+          serial_rx_buffer_controller[10] = 0x00;
 
           for (serial_rx_buffer_counter = 0; serial_rx_buffer_counter < sizeof(serial_rx_buffer_controller); serial_rx_buffer_counter++) {
             Serial.write(serial_rx_buffer_controller[serial_rx_buffer_counter]); //  This line writes the serial data back to the computer as a way to check if the Arduino isn't interpreting wrong values
